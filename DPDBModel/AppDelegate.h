f81138bbd80dea4053a58a1c889cf112290ec3f1
人众 <renzhong@renzhongdeiMac.local>
2018-06-14 16:22:11 +0800

    第一次更新

---
+++ DPDBModel/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  DPDBModel
+//
+//  Created by 人众 on 2018/6/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
